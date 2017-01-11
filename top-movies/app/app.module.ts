5584aee9ca0912e33ecf61e73ae96a9372caab0c
yuletodim <yuletodim@yahoo.es>
2017-01-11 10:47:22 +0200

    set app module

--- top-movies/app/app.module.ts
+++ top-movies/app/app.module.ts
@@ -4,9 +4,16 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
+//import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
-    // put all the needed data here
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers:[],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
